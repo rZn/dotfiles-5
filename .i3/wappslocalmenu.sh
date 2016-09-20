4d0b866cacffa727124d329eb43f7aeb6cd46251
eoli3n <jonathan.kirszling@umontpellier.fr>
2016-09-20 18:04:49 +0200

    changed menu

--- .i3/wappslocalmenu.sh
+++ .i3/wappslocalmenu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-action=$(echo -e "ompd\nsoundcloud\ngmaps\nnetflix\ntransmission" | dmenu -fn 'hack-10' -h 20 -nb '#202020' -nf '#FFFFFF' -sf '#dc322f' -sb '#202020' -p "wapps >" -w 190 -o 0.9 -y 21 -l 2)
+action=$(echo -e "ompd\ntransmission" | dmenu -fn 'hack-10' -h 20 -nb '#202020' -nf '#FFFFFF' -sf '#dc322f' -sb '#202020' -p "wapps >" -w 190 -o 0.9 -y 21 -l 2)
 
 if [[ "$action" == "ompd" ]]
 then
