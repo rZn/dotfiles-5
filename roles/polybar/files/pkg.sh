15e74141574d8aee6a2d0879774d98c5749cb24d
eoli3n <jonathan.kirszling@umontpellier.fr>
2018-01-07 22:32:25 +0100

    added fzf

--- roles/polybar/files/pkg.sh
+++ roles/polybar/files/pkg.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 pac=$(checkupdates | wc -l)
 aur=$(cower -u | wc -l)
+kernel=$(vercmp "$(uname -r | cut -f-2 -d-)"  "$(pacman --nodeps -Sp --print-format %v linux)")
 
-check=$((pac + aur))
-if [[ "$check" != "0" ]]
+if [[ $kernel == "0" ]]
 then
     echo "$pac %{F#5b5b5b}%{F-} $aur"
+else
+    echo "$pac %{F#ff5c57}%{F-} $aur"
 fi
