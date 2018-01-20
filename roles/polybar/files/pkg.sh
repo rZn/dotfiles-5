bd3bdecc764b33e782618b57586d7c76bdac56d9
eoli3n <jonathan.kirszling@umontpellier.fr>
2018-01-20 13:07:45 +0100

    improve polybar pkg.shé

--- roles/polybar/files/pkg.sh
+++ roles/polybar/files/pkg.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-pac=$(checkupdates | wc -l)
-aur=$(cower -u | wc -l)
+pac=$(checkupdates | wc -l) 2>/dev/null
+aur=$(cower -u | wc -l) 2>/dev/null
 kernel=$(vercmp "$(uname -r | cut -f-2 -d-)"  "$(pacman --nodeps -Sp --print-format %v linux)")
+
+if [ $pac -eq "0" ] && [ $aur -eq "0" ]
+then
+    exit 0
+fi
 
 if [[ $kernel == "0" ]]
 then
