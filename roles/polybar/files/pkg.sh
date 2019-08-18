72d186bddc3036acf1e272967a234e87340b37d3
eoli3n <jonathan.kirszling@umontpellier.fr>
2019-08-18 12:52:40 +0200

    removed cower dependency

--- roles/polybar/files/pkg.sh
+++ roles/polybar/files/pkg.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-pac=$(checkupdates | wc -l)
-aur=$(cower -u | wc -l)
+pac=$(checkupdates 2>/dev/null | wc -l)
+aur=$(cower -u 2>/dev/null | wc -l)
+kernel=$(vercmp "$(uname -r | cut -f-2 -d-)"  "$(pacman --nodeps -Sp --print-format %v linux)")
 
-check=$((pac + aur))
-if [[ "$check" != "0" ]]
+if [[ $pac -eq "0" ]] && [[ $aur -eq "0" ]] || [[ ! $pac =~ [0-9]+ ]] || [[ ! $aur =~ [0-9]+ ]]
+then
+    exit 0
+fi
+
+if [[ $kernel == "0" ]]
 then
     echo "$pac %{F#5b5b5b}%{F-} $aur"
+else
+    echo "$pac %{F#ff5c57}%{F-} $aur"
 fi
