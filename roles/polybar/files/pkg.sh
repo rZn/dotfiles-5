03737fbac5d0beba30a57ebcd7f9d59dbada6bb0
eoli3n <jonathan.kirszling@umontpellier.fr>
2019-12-28 09:59:55 +0100

    playerctl and wireless updown

--- roles/polybar/files/pkg.sh
+++ roles/polybar/files/pkg.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pac=$(checkupdates 2>/dev/null | wc -l)
 aur=$(trizen -Qua 2>/dev/null | wc -l)
-#kernel=$(vercmp "$(uname -r | sed 's/-arch/\./')"  "$(pacman --nodeps -Sp --print-format %v linux)")
 
 if [[ $pac -eq "0" ]] && [[ $aur -eq "0" ]] || [[ ! $pac =~ [0-9]+ ]] || [[ ! $aur =~ [0-9]+ ]]
 then
