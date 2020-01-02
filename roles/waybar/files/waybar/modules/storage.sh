b1d72f9b2756a7cc3b3bfa6dca93f0ac73969bef
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-01-02 12:00:25 +0100

    waybar storage module

--- roles/waybar/files/waybar/modules/storage.sh
+++ roles/waybar/files/waybar/modules/storage.sh
@@ -1,24 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-mount="/"
-warning=20
-critical=10
+warning=85
+critical=95
 
-df -h -P -l "$mount" | awk -v warning=$warning -v critical=$critical '
-/\/.*/ {
-  text=$4
-  tooltip="Filesystem: "$1"\rSize: "$2"\rUsed: "$3"\rAvail: "$4"\rUse%: "$5"\rMounted on: "$6
-  use=$5
-  exit 0
-}
-END {
-  class=""
-  gsub(/%$/,"",use)
-  if ((100 - use) < critical) {
-    class="critical"
-  } else if ((100 - use) < warning) {
-    class="warning"
-  }
-  print "{\"text\":\""text"\", \"percentage\":"use",\"tooltip\":\""tooltip"\", \"class\":\""class"\"}"
-}
-'
+pcent=$(df "$1" --output="pcent" 2>/dev/null | sed 1d | tr -d ' ' | tr -d '%')
+json_fmt='{"text":"%s","class":"%s"}\n'
+
+if [[ -z $pcent ]]
+then
+    echo -e "Usage: ./storage.sh [mountpoint]\nexemple : ./storage.sh /"
+else
+    if [[ $pcent -lt $warning ]]
+    then
+        printf "$json_fmt" "$pcent" "normal"
+    elif [[ $pcent -ge $warning ]] && [[ $pcent -lt $critical ]]
+    then
+        printf "$json_fmt" "$pcent" "warning"
+    elif [[ $pcent -ge $critical ]]
+    then
+      printf "$json_fmt" "$pcent" "critical"
+    fi
+fi
