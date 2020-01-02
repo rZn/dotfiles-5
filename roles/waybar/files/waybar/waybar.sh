4342d8328bf015c7f52ca824e5966130dece82ee
eoli3n <jkirsz@gmail.com>
2020-01-02 18:21:11 +0100

    change killall to pkill

--- roles/waybar/files/waybar/waybar.sh
+++ roles/waybar/files/waybar/waybar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q waybar
+pkill waybar
 
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
