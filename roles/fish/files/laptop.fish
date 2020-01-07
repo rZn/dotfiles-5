2640ad0d537355e13c5b7477774b9d869695b7da
eoli3n <jkirsz@gmail.com>
2020-01-07 16:47:09 +0100

    fish workaround tray

--- roles/fish/files/laptop.fish
+++ roles/fish/files/laptop.fish
@@ -2,4 +2,4 @@ set -Ua fish_user_paths $HOME/bin
 set -Ua fish_user_paths $HOME/go/bin
 set -x BROWSER 'firefox'
 set -x VAGRANT_DEFAULT_PROVIDER 'libvirt'
-set -x XDG_CURRENT_DESKTOP 'sway'
+set -x XDG_CURRENT_DESKTOP 'Unity' # waybar workaround for tray
