20b14f2db1647c20cb416a29a3a9b195e32e5578
eoli3n <jonathan.kirszling@umontpellier.fr>
2020-04-25 11:39:51 +0200

    added fish path

--- roles/fish/files/laptop.fish
+++ roles/fish/files/laptop.fish
@@ -1,5 +1,6 @@
 set -Ua fish_user_paths $HOME/bin
 set -Ua fish_user_paths $HOME/go/bin
+set -Ua fish_user_paths $HOME/.gem/ruby/2.7.0/bin
 set -x BROWSER 'firefox'
 set -x VAGRANT_DEFAULT_PROVIDER 'libvirt'
 set -x XDG_CURRENT_DESKTOP 'Unity' # waybar workaround for tray
