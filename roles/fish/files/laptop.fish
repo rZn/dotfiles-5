fef0652cb65e09a8d8fe0914430245819f4e4fca
eoli3n <jkirsz@gmail.com>
2020-05-08 11:27:41 +0200

    updated fish config

--- roles/fish/files/laptop.fish
+++ roles/fish/files/laptop.fish
@@ -1,6 +1,3 @@
-set -Ua fish_user_paths $HOME/bin
-set -Ua fish_user_paths $HOME/go/bin
-set -Ua fish_user_paths $HOME/.gem/ruby/2.7.0/bin
+set -U fish_user_paths $HOME/bin $HOME/go/bin $HOME/.gem/ruby/2.7.0/bin
 set -x BROWSER 'firefox'
 set -x VAGRANT_DEFAULT_PROVIDER 'libvirt'
-set -x XDG_CURRENT_DESKTOP 'Unity' # waybar workaround for tray
--- roles/sway/files/sway.fish
+++ roles/sway/files/sway.fish
@@ -1,5 +1,7 @@
 # Autostart sway when user login on tty1
 
+set -x XDG_CURRENT_DESKTOP 'Unity' # waybar workaround for tray
+
 set TTY1 (tty)
 
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
