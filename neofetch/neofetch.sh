71d33fc8e7293cf5315990481da1fe8336b85074
eoli3n <jkirsz@gmail.com>
2020-05-11 14:55:19 +0200

    updated wofi and neofetch

--- neofetch/neofetch.sh
+++ neofetch/neofetch.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 wget "https://avatars0.githubusercontent.com/u/12347044?s=400&v=4" -O /tmp/archzfs.png
 
-neofetch --config "$DIR"/config --kitty /tmp/archzfs.png
+neofetch --config "$DIR"/config --kitty /tmp/archzfs.png --size 630px
