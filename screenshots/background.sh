e66178c8d3f48fa0b075695b8109850b09e23aff
eoli3n <jkirsz@gmail.com>
2020-05-13 22:21:24 +0200

    removed background script

--- screenshots/background.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-background="https://unsplash.com/photos/wQLAGv4_OYs/download?force=true&w=1920"
-
-wget "$background" -O /tmp/background.jpg
-
-swaymsg output "*" bg /tmp/background.jpg fill
