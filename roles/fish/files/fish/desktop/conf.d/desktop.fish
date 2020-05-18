03b3aaa86a13217d00fee5d3bf68e7643a53ba9e
eoli3n <jkirsz@gmail.com>
2020-05-18 23:19:00 +0200

    added mpv config for wayland ha

--- roles/fish/files/fish/desktop/conf.d/desktop.fish
+++ roles/fish/files/fish/desktop/conf.d/desktop.fish
@@ -4,4 +4,5 @@ if test "$hostname" = "osz"
     set -x fish_user_paths $HOME/bin $HOME/go/bin $HOME/.gem/ruby/2.7.0/bin
 end
 
+set -gx LIBVA_DRIVER_NAME 'iHD'
 set -x VAGRANT_DEFAULT_PROVIDER 'libvirt'
